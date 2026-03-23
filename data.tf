371406f0f48e1bb8153a66bd6a52bafdb6eda070
hyma999 <hymaaritakula93@gmail.com>
2026-03-23 12:50:49 +0530

    added

--- data.tf
+++ data.tf
@@ -2,15 +2,15 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
-data "aws_vpc" "default" {
-  default = true
-}
+# data "aws_vpc" "default" {
+#   default = true
+# }
 
-data "aws_route_table" "default" {
-  vpc_id = data.aws_vpc.default.id
+# data "aws_route_table" "default" {
+#   vpc_id = data.aws_vpc.default.id
 
-  filter {
-    name   = "association.main"
-    values = ["true"]
-  }
-}
+#   filter {
+#     name   = "association.main"
+#     values = ["true"]
+#   }
+# }
